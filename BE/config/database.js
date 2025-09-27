db73dc5bce47a93a6b630360632a5d12b658663e
Luis Larghi <windinglair@gmail.com>
2025-09-26 20:48:11 -0400

    Modify mongoose connection

--- BE/config/database.js
+++ BE/config/database.js
@@ -1,9 +1,13 @@
-import { connect } from "mongoose";
+import {connect} from "mongoose";
 import 'dotenv/config';
 
 export const connectDB = async () => {
     try {
-        await connect(process.env.MONGODB_URI);
+        await connect(process.env.MONGODB_URI, {
+            retryWrites: true,
+            writeConcern: "majority",
+            appName: process.env.MONGODB_APP_NAME,
+        });
         console.log('MongoDB Connected');
     } catch (error) {
         console.log(error);
