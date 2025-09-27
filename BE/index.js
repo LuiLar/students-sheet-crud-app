7b15de01ae26b735b732c629b592965e191e8c9e
Luis Larghi <windinglair@gmail.com>
2025-09-27 00:18:41 -0400

    Ditto

--- BE/index.js
+++ BE/index.js
@@ -2,12 +2,12 @@ import app from './app.js';
 import { connectDB } from './config/database.js';
 import 'dotenv/config';
 
-connectDB();
-
-app.listen(process.env.PORT, (err) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(`Server started on port ${process.env.PORT}`);
-    }
+connectDB().then(() => {
+    app.listen(process.env.PORT, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(`Server started on port ${process.env.PORT}`);
+        }
+    })
 })
